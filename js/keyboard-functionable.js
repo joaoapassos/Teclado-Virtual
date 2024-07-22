c2f345b31c8f83e70a356085854cf4074887ca5d
João A. Passos <112397879+joaoapassos@users.noreply.github.com>
2024-07-22 01:07:19 -0300

    Versão 0.1.0
    Numa só eu terminei todo o app, mas ainda sujeito a bugs, revisar o codigo antes de finalizar.

--- js/keyboard-functionable.js
+++ js/keyboard-functionable.js
@@ -10,30 +10,75 @@ function keyboardFunctionable (){
                 textarea.value = `${textarea.value}${key}`;
             }
             else if(key.id == 'key-caps'){
-                if(!capsOn){
-                    document.querySelectorAll('.keys').forEach((key) => {
-                        if(key.id !== 'key-caps' && key.id !== 'key-back' && key.id !== 'key-space'){
-                            key.innerText = key.innerText.toUpperCase();
-                        }
-                        capsOn = true
-                    });
-                }
-                else{
-                    document.querySelectorAll('.keys').forEach((key) => {
-                        if(key.id !== 'key-caps' && key.id !== 'key-back' && key.id !== 'key-space'){
-                            key.innerText = key.innerText.toLowerCase();
-                        }
-                        capsOn = false
-                    });
-                }
+                keyCaps();
             }
             else if(key.id == 'key-back'){
-                textarea.value = textarea.value.slice(0, -1);
+                var start = textarea.selectionStart;
+                var end = textarea.selectionEnd;
+
+                if (start === end) {
+                    if (start > 0) {
+                        textarea.value = textarea.value.slice(0, start - 1) + textarea.value.slice(end);
+                        textarea.selectionStart = textarea.selectionEnd = start - 1;
+                    }
+                } else {
+                    textarea.value = textarea.value.slice(0, start) + textarea.value.slice(end);
+                    textarea.selectionStart = textarea.selectionEnd = start;
+                }
             }
             else if(key.id == 'key-space'){
                 textarea.value = `${textarea.value} `;
             }
+            textarea.focus();
+            })
+    })
+    document.addEventListener('keydown', (e)=>{
+        keys.forEach((key)=>{
+            if(e.key == key.name && e.key !== 'CapsLock'){ 
+                textarea.focus();
+                key.classList.add('keys-on')
+                setTimeout(
+                    ()=>{
+                        key.classList.remove('keys-on')
+                    }, 500
+                )
+            }
+            else if(e.key == 'CapsLock'){
+                textarea.focus();
+                keyCaps();
+            }
         })
     })
+    function keyCaps(){
+        if(!capsOn){
+            document.querySelectorAll('.keys').forEach((key) => {
+                if(key.id !== 'key-caps' && key.id !== 'key-back' && key.id !== 'key-space'){
+                    key.innerText = key.innerText.toUpperCase();
+                };
+                if(key.id == 'key-caps'){
+                    key.classList.add('keys-on');
+                }
+                
+                capsOn = true
+            });
+        }
+        else{
+            document.querySelectorAll('.keys').forEach((key) => {
+                if(key.id !== 'key-caps' && key.id !== 'key-back' && key.id !== 'key-space'){
+                    key.innerText = key.innerText.toLowerCase();
+                };
+                if(key.id == 'key-caps'){
+                    key.classList.remove('keys-on');
+                }
+                capsOn = false
+            });
+        }
+    }
 }
-keyboardFunctionable();
+function load(){
+    keyboardFunctionable();
+    textarea.focus();
+    document.getElementById('svg-key-back').addEventListener('click', ()=>{
+        console.log(this)
+    });
+}
